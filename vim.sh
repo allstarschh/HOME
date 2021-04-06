9eb1b6305850d3b503253a06fc75b9bc90b7bdf3
Yoshi Cheng-Hao Huang <allstars.chh@gmail.com>
2021-04-06 23:30:45 +0200

    add vim lightline

--- vim.sh
+++ vim.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+
+# Vim packages
+mkdir -p ~/.vim/pack/plugins/start/
+git clone https://github.com/itchyny/lightline.vim ~/.vim/pack/plugins/start/
+
 mkdir -p ~/.vim/bundle
 cd ~/.vim/bundle
 git clone git://github.com/godlygeek/tabular.git
