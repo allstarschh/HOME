c90da2bbdc5af2dfb8f6ddb90233ac686ecd94a2
Yoshi Cheng-Hao Huang <allstars.chh@gmail.com>
2021-04-07 21:52:27 +0200

    add auto-pairs

--- vim.sh
+++ vim.sh
@@ -3,6 +3,7 @@
 # Vim packages
 mkdir -p ~/.vim/pack/plugins/start/
 git clone https://github.com/itchyny/lightline.vim ~/.vim/pack/plugins/start/
+git clone git://github.com/jiangmiao/auto-pairs.git ~/.vim/pack/plugins/start/
 
 mkdir -p ~/.vim/bundle
 cd ~/.vim/bundle
